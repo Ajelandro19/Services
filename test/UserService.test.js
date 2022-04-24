5d7a2d23b90ea31df4415edf9f4ca4714aef9795
Ajelandro19 <almon190699@gmail.com>
2022-04-23 21:00:13 -0500

    Paso 2: Creamos una prueba con los requerimientos y verificamos que falle

---
+++ test/UserService.test.js
@@ -0,0 +1,14 @@
+const { TestWatcher } = require('jest')
+const UserService = require('./../app/services/UserService')
+
+describe('Test for UserService',()=>{
+    test("1. Create a new user using the UserService", ()=>{
+        const user= UserService.create(1, "AjelandroMon", "Ajelandro")
+        expect(user.username).toBe("AjelandroMon")
+        expect(user.name).toBe("Ajelandro")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined
+        
+    })   
+
+})
