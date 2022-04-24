0a86442fbc15408235365a2a0fe8a581de5319ad
Ajelandro19 <almon190699@gmail.com>
2022-04-23 22:22:21 -0500

    Paso 4: Agregamos requerimiento 2 y su solucion

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,12 @@
+
 const User = require('./../modules/user')
 class UserService{
     static create(id, username, name){
         return new User(id, username,name, "Sin bio")
+    }
+    static getInfo(user){
+        const userInfoInList=[user.id, user.username,user.name, "Sin bio"]
+        return userInfoInList
     }
 }
 
--- test/UserService.test.js
+++ test/UserService.test.js
@@ -8,6 +8,16 @@ describe('Test for UserService',()=>{
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined
         
+    })
+    test("2. Get all user data in a list", ()=>{
+        const user= UserService.create(1, "AjelandroMon", "Ajelandro")
+        const userInfoInList = UserService.getInfo(user)
+        console.log(userInfoInList)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("AjelandroMon")
+        expect(userInfoInList[2]).toBe("Ajelandro")
+        expect(userInfoInList[3]).toBe("Sin bio")
+        
     })   
 
 })
